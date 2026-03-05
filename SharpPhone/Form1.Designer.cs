a7a0441f7ea5c1ce7db015b5bd556a3702a1b3f3
Robert Postma <205792@novacollege.nl>
2026-03-05 10:51:21 +0100

    Added second form and connnected it to the first form

--- SharpPhone/Form1.Designer.cs
+++ SharpPhone/Form1.Designer.cs
@@ -31,6 +31,7 @@
             listBox1 = new ListBox();
             btnAddPhone = new Button();
             btnModify = new Button();
+            btnDelete = new Button();
             SuspendLayout();
             // 
             // listBox1
@@ -39,12 +40,12 @@
             listBox1.ItemHeight = 16;
             listBox1.Location = new Point(3, 0);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(475, 484);
+            listBox1.Size = new Size(344, 484);
             listBox1.TabIndex = 0;
             // 
             // btnAddPhone
             // 
-            btnAddPhone.Location = new Point(484, 12);
+            btnAddPhone.Location = new Point(353, 12);
             btnAddPhone.Name = "btnAddPhone";
             btnAddPhone.Size = new Size(94, 29);
             btnAddPhone.TabIndex = 1;
@@ -54,19 +55,31 @@
             // 
             // btnModify
             // 
-            btnModify.Location = new Point(484, 47);
+            btnModify.Location = new Point(353, 47);
             btnModify.Name = "btnModify";
             btnModify.RightToLeft = RightToLeft.Yes;
             btnModify.Size = new Size(94, 29);
             btnModify.TabIndex = 2;
             btnModify.Text = "Aanpassen";
             btnModify.UseVisualStyleBackColor = true;
+            btnModify.Click += btnModify_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(353, 82);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Verwijderen";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
             // 
             // listPhones
             // 
             AutoScaleDimensions = new SizeF(8F, 16F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(887, 485);
+            ClientSize = new Size(459, 485);
+            Controls.Add(btnDelete);
             Controls.Add(btnModify);
             Controls.Add(btnAddPhone);
             Controls.Add(listBox1);
@@ -80,5 +93,6 @@
         private ListBox listBox1;
         private Button btnAddPhone;
         private Button btnModify;
+        private Button btnDelete;
     }
 }
--- SharpPhone/Form1.cs
+++ SharpPhone/Form1.cs
@@ -5,12 +5,23 @@ namespace SharpPhone
         public listPhones()
         {
             InitializeComponent();
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            Form frmAddEdit = new frmAddEdit();
+            frmAddEdit.ShowDialog();
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click");
         }
     }
 }
